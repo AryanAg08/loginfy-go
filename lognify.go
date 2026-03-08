2fa4b8c00b1c38c2e2a55ad8f9183c81c29c593e
Aryan Goyal <aryangoyal90@outlook.com>
2026-03-09 01:29:20 +0530

    Adding all function usage

--- lognify.go
+++ lognify.go
@@ -5,6 +5,8 @@
 //
 // Quick Start:
 //
+//	import lognify "github.com/AryanAg08/loginfy-go"
+//
 //	auth := lognify.New()
 //	auth.Use(emailpassword.New())
 //	auth.SetStorage(memory.New())
@@ -14,10 +16,40 @@
 package lognify
 
 import (
+	"github.com/AryanAg08/loginfy-go/authorization"
 	"github.com/AryanAg08/loginfy-go/core"
 )
 
 // New creates a new Lognify authentication instance.
 func New() *core.Loginfy {
 	return core.New()
+}
+
+// NewAuthorization creates a new RBAC authorization manager.
+func NewAuthorization() *authorization.Authorizer {
+	return authorization.New()
+}
+
+// WithJWTSecret is a convenience option type for configuring JWT secret.
+type Option func(*core.Loginfy)
+
+// Configure applies options to a Loginfy instance.
+func Configure(l *core.Loginfy, opts ...Option) {
+	for _, opt := range opts {
+		opt(l)
+	}
+}
+
+// WithStorage returns an option that sets the storage adapter.
+func WithStorage(s core.Storage) Option {
+	return func(l *core.Loginfy) {
+		l.SetStorage(s)
+	}
+}
+
+// WithSessionManager returns an option that sets the session manager.
+func WithSessionManager(sm core.SessionManager) Option {
+	return func(l *core.Loginfy) {
+		l.SetSessionManager(sm)
+	}
 }
